2307ddf35d3ac910aa1e799c636b253a0a21bbeb
Preslav Kolev <kolevpreslav91@gmail.com>
2026-07-08 16:46:35 +0300

    Create an index.html for testing

--- online_store/products/views.py
+++ online_store/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product, Category
 
-# Create your views here.
+
+def index(request):
+    # Вземаме всички активни продукти
+    products = Product.objects.filter(is_active=True).prefetch_related('variants__media')
+    categories = Category.objects.all()
+
+    context = {
+        'products': products,
+        'categories': categories,
+    }
+    return render(request, 'products/index.html', context)
